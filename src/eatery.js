627263fb65e81ea48eeb588550f6a23bb9bae8d2
kevin park <kevin.park02@gmail.com>
2021-01-05 23:38:50 -0800

    Make the scroll function work for all schools

--- src/eatery.js
+++ src/eatery.js
@@ -1,12 +1,34 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     document.querySelector(".davis-get-restaurants").addEventListener('click', getRestaurants);
     document.querySelector(".merced-get-restaurants").addEventListener('click', getRestaurants);
+    document.querySelector(".berkeley-get-restaurants").addEventListener('click', getRestaurants);
+    document.querySelector(".sc-get-restaurants").addEventListener('click', getRestaurants);
+    document.querySelector(".sb-get-restaurants").addEventListener('click', getRestaurants);
+    document.querySelector(".la-get-restaurants").addEventListener('click', getRestaurants);
+    document.querySelector(".riverside-get-restaurants").addEventListener('click', getRestaurants);
+    document.querySelector(".irvine-get-restaurants").addEventListener('click', getRestaurants);
+    document.querySelector(".sd-get-restaurants").addEventListener('click', getRestaurants);
 
     function getRestaurants() {
         let davisRestaurants = document.querySelector(".davis-restaurants");
             davisRestaurants.classList.add("restaurant-appear");
         let mercedRestaurants = document.querySelector(".merced-restaurants");
-            mercedRestaurants.classList.add('restaurant-appear')
+            mercedRestaurants.classList.add('restaurant-appear');
+        let berkeleyRestaurants = document.querySelector(".berkeley-restaurants");
+            berkeleyRestaurants.classList.add('restaurant-appear');
+        let scRestaurants = document.querySelector(".sc-restaurants");
+            scRestaurants.classList.add('restaurant-appear');
+        let sbRestaurants = document.querySelector(".sb-restaurants");
+            sbRestaurants.classList.add('restaurant-appear');
+        let laRestaurants = document.querySelector(".la-restaurants");
+            laRestaurants.classList.add('restaurant-appear');
+        let riversideRestaurants = document.querySelector(".riverside-restaurants");
+            riversideRestaurants.classList.add('restaurant-appear');
+        let irvineRestaurants = document.querySelector(".irvine-restaurants");
+            irvineRestaurants.classList.add('restaurant-appear');
+        let sdRestaurants = document.querySelector(".sd-restaurants");
+            sdRestaurants.classList.add('restaurant-appear');
+
 
     }
 })
--- src/info.js
+++ src/info.js
@@ -2,19 +2,32 @@ window.addEventListener("DOMContentLoaded", (event) => {
     window.onscroll = function () { scrollFunction(); };
 
     function scrollFunction() {
-        let davis = document.getElementById("davis")
+        let davis = document.getElementById("davis");
         let davisRestaurants = document.querySelector(".davis-restaurants");
 
-        let merced = document.getElementById("merced")
+        let merced = document.getElementById("merced");
         let mercedRestaurants = document.querySelector(".merced-restaurants");
+        
+        let berkeley = document.getElementById("berkeley");
+        let berkeleyRestaurants = document.querySelector(".berkeley-restaurants");
+        
+        let santacruz = document.getElementById("santacruz");
+        let scRestaurants = document.querySelector(".sc-restaurants");
+        
+        let santabarbara = document.getElementById("santabarbara");
+        let sbRestaurants = document.querySelector(".sb-restaurants");
+        
+        let ucla = document.getElementById("ucla");
+        let laRestaurants = document.querySelector(".la-restaurants");
 
-        let berkeley = document.getElementById("berkeley")
-        let santacruz = document.getElementById("santacruz")
-        let santabarbara = document.getElementById("santabarbara")
-        let ucla = document.getElementById("ucla")
-        let riverside = document.getElementById("riverside")
-        let irvine = document.getElementById("irvine")
-        let sandiego = document.getElementById("sandiego")
+        let riverside = document.getElementById("riverside");
+        let riversideRestaurants = document.querySelector(".riverside-restaurants");
+
+        let irvine = document.getElementById("irvine");
+        let irvineRestaurants = document.querySelector(".irvine-restaurants");
+        
+        let sandiego = document.getElementById("sandiego");
+        let sdRestaurants = document.querySelector(".sd-restaurants");
 
         if(document.documentElement.scrollTop < 449) {
             davis.classList.add("appear");
@@ -42,6 +55,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
             merced.classList.add("appear");
 
             berkeley.classList.remove("appear");
+            berkeleyRestaurants.classList.remove("restaurant-appear");
 
             santacruz.classList.remove("appear");
 
@@ -63,6 +77,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
             berkeley.classList.add("appear");
 
             santacruz.classList.remove("appear");
+            scRestaurants.classList.remove("restaurant-appear");
 
             santabarbara.classList.remove("appear");
 
@@ -79,10 +94,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
             merced.classList.remove("appear");
 
             berkeley.classList.remove("appear");
+            berkeleyRestaurants.classList.remove("restaurant-appear");
 
             santacruz.classList.add("appear");
 
             santabarbara.classList.remove("appear");
+            sbRestaurants.classList.remove("restaurant-appear");
 
             ucla.classList.remove("appear");
 
@@ -99,10 +116,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
             berkeley.classList.remove("appear");
 
             santacruz.classList.remove("appear");
+            scRestaurants.classList.remove("restaurant-appear");
 
             santabarbara.classList.add("appear");
 
             ucla.classList.remove("appear");
+            laRestaurants.classList.remove("restaurant-appear");
 
             riverside.classList.remove("appear");
 
@@ -119,10 +138,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
             santacruz.classList.remove("appear");
 
             santabarbara.classList.remove("appear");
+            sbRestaurants.classList.remove("restaurant-appear");
 
             ucla.classList.add("appear");
 
             riverside.classList.remove("appear");
+            riversideRestaurants.classList.remove("restaurant-appear");
 
             irvine.classList.remove("appear");
 
@@ -139,10 +160,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
             santabarbara.classList.remove("appear");
 
             ucla.classList.remove("appear");
+            laRestaurants.classList.remove("restaurant-appear");
 
             riverside.classList.add("appear");
 
             irvine.classList.remove("appear");
+            irvineRestaurants.classList.remove("restaurant-appear");
 
             sandiego.classList.remove("appear");
         } else if ((document.documentElement.scrollTop < 5199) && (document.documentElement.scrollTop > 4520)) {
@@ -159,10 +182,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
             ucla.classList.remove("appear");
 
             riverside.classList.remove("appear");
-
+            riversideRestaurants.classList.remove("restaurant-appear");
+            
             irvine.classList.add("appear");
-
+            
             sandiego.classList.remove("appear");
+            sdRestaurants.classList.remove("restaurant-appear");
         } else if ((document.documentElement.scrollTop < 7000) && (document.documentElement.scrollTop > 5200)) {
             davis.classList.remove("appear");
 
@@ -179,6 +204,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
             riverside.classList.remove("appear");
 
             irvine.classList.remove("appear");
+            irvineRestaurants.classList.remove("restaurant-appear");
 
             sandiego.classList.add("appear");
         }
