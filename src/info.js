0203dbae522809a691fa58f358dd2af1d0901708
kevin park <kevin.park02@gmail.com>
2021-01-07 11:18:17 -0800

    Make the side bar icons fade in and out based on the scroll top position

--- src/info.js
+++ src/info.js
@@ -1,49 +1,65 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+    let davis = document.getElementById("davis");
+    davis.classList.add("appear");
+    let davisIcon = document.getElementById("davis-icon");
+    davisIcon.classList.add("highlight");
+
     window.onscroll = function () { scrollFunction(); };
 
     function scrollFunction() {
         let davis = document.getElementById("davis");
         let davisRestaurants = document.querySelector(".davis-restaurants");
         let davisYelp = document.querySelector(".davis-yelp");
+        let davisIcon = document.getElementById("davis-icon");
 
         let merced = document.getElementById("merced");
         let mercedRestaurants = document.querySelector(".merced-restaurants");
         let mercedYelp = document.querySelector(".merced-yelp");
+        let mercedIcon = document.getElementById("merced-icon");
         
         let berkeley = document.getElementById("berkeley");
         let berkeleyRestaurants = document.querySelector(".berkeley-restaurants");
         let berkeleyYelp = document.querySelector(".berkeley-yelp");
+        let berkeleyIcon = document.getElementById("berkeley-icon");
         
         let santacruz = document.getElementById("santacruz");
         let scRestaurants = document.querySelector(".sc-restaurants");
         let scYelp = document.querySelector(".sc-yelp");
+        let scIcon = document.getElementById("sc-icon");
         
         let santabarbara = document.getElementById("santabarbara");
         let sbRestaurants = document.querySelector(".sb-restaurants");
         let sbYelp = document.querySelector(".sb-yelp");
+        let sbIcon = document.getElementById("sb-icon");
         
         let ucla = document.getElementById("ucla");
         let laRestaurants = document.querySelector(".la-restaurants");
         let laYelp = document.querySelector(".la-yelp");
+        let laIcon = document.getElementById("la-icon");
         
         let riverside = document.getElementById("riverside");
         let riversideRestaurants = document.querySelector(".riverside-restaurants");
         let riversideYelp = document.querySelector(".riverside-yelp");
+        let riversideIcon = document.getElementById("riverside-icon");
         
         let irvine = document.getElementById("irvine");
         let irvineRestaurants = document.querySelector(".irvine-restaurants");
         let irvineYelp = document.querySelector(".irvine-yelp");
+        let irvineIcon = document.getElementById("irvine-icon");
         
         let sandiego = document.getElementById("sandiego");
         let sdRestaurants = document.querySelector(".sd-restaurants");
         let sdYelp = document.querySelector(".sd-yelp");
+        let sdIcon = document.getElementById("sd-icon");
 
         if(document.documentElement.scrollTop < 449) {
             davis.classList.add("appear");
+            davisIcon.classList.add("highlight")
 
             merced.classList.remove("appear");
             mercedRestaurants.classList.remove("restaurant-appear");
             mercedYelp.classList.remove("yelp-appear");
+            mercedIcon.classList.remove("highlight");
 
             berkeley.classList.remove("appear");
 
@@ -62,12 +78,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
             davis.classList.remove("appear");
             davisRestaurants.classList.remove("restaurant-appear");
             davisYelp.classList.remove("yelp-appear");
+            davisIcon.classList.remove("highlight");
 
             merced.classList.add("appear");
+            mercedIcon.classList.add("highlight")
 
             berkeley.classList.remove("appear");
             berkeleyRestaurants.classList.remove("restaurant-appear");
             berkeleyYelp.classList.remove("yelp-appear");
+            berkeleyIcon.classList.remove("highlight");
 
             santacruz.classList.remove("appear");
 
@@ -86,12 +105,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
             merced.classList.remove("appear");
             mercedRestaurants.classList.remove("restaurant-appear");
             mercedYelp.classList.remove("yelp-appear");
+            mercedIcon.classList.remove("highlight");
 
             berkeley.classList.add("appear");
+            berkeleyIcon.classList.add("highlight");
 
             santacruz.classList.remove("appear");
             scRestaurants.classList.remove("restaurant-appear");
             scYelp.classList.remove("yelp-appear");
+            scIcon.classList.remove("highlight");
 
             santabarbara.classList.remove("appear");
 
@@ -110,12 +132,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
             berkeley.classList.remove("appear");
             berkeleyRestaurants.classList.remove("restaurant-appear");
             berkeleyYelp.classList.remove("yelp-appear");
+            berkeleyIcon.classList.remove("highlight");
 
             santacruz.classList.add("appear");
+            scIcon.classList.add("highlight");
 
             santabarbara.classList.remove("appear");
             sbRestaurants.classList.remove("restaurant-appear");
             sbYelp.classList.remove("yelp-appear");
+            sbIcon.classList.remove("highlight");
 
             ucla.classList.remove("appear");
 
@@ -133,13 +158,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
             santacruz.classList.remove("appear");
             scRestaurants.classList.remove("restaurant-appear");
-            sbYelp.classList.remove("yelp-appear");
+            scYelp.classList.remove("yelp-appear");
+            scIcon.classList.remove("highlight");
 
             santabarbara.classList.add("appear");
+            sbIcon.classList.add("highlight");
 
             ucla.classList.remove("appear");
             laRestaurants.classList.remove("restaurant-appear");
             laYelp.classList.remove("yelp-appear");
+            laIcon.classList.remove("highlight");
 
             riverside.classList.remove("appear");
 
@@ -158,12 +186,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
             santabarbara.classList.remove("appear");
             sbRestaurants.classList.remove("restaurant-appear");
             sbYelp.classList.remove("yelp-appear");
+            sbIcon.classList.remove("highlight");
 
             ucla.classList.add("appear");
+            laIcon.classList.add("highlight");
 
             riverside.classList.remove("appear");
             riversideRestaurants.classList.remove("restaurant-appear");
             riversideYelp.classList.remove("yelp-appear");
+            riversideIcon.classList.remove("highlight");
 
             irvine.classList.remove("appear");
 
@@ -182,12 +213,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
             ucla.classList.remove("appear");
             laRestaurants.classList.remove("restaurant-appear");
             laYelp.classList.remove("yelp-appear");
+            laIcon.classList.remove("highlight");
 
             riverside.classList.add("appear");
+            riversideIcon.classList.add("highlight");
 
             irvine.classList.remove("appear");
             irvineRestaurants.classList.remove("restaurant-appear");
             irvineYelp.classList.remove("yelp-appear");
+            irvineIcon.classList.remove("highlight");
 
             sandiego.classList.remove("appear");
         } else if ((document.documentElement.scrollTop < 5199) && (document.documentElement.scrollTop > 4520)) {
@@ -206,12 +240,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
             riverside.classList.remove("appear");
             riversideRestaurants.classList.remove("restaurant-appear");
             riversideYelp.classList.remove("yelp-appear");
+            riversideIcon.classList.remove("highlight");
             
             irvine.classList.add("appear");
+            irvineIcon.classList.add("highlight");
             
             sandiego.classList.remove("appear");
             sdRestaurants.classList.remove("restaurant-appear");
             sdYelp.classList.remove("yelp-appear");
+            sdIcon.classList.remove("highlight")
         } else if ((document.documentElement.scrollTop < 7000) && (document.documentElement.scrollTop > 5200)) {
             davis.classList.remove("appear");
 
@@ -230,8 +267,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
             irvine.classList.remove("appear");
             irvineRestaurants.classList.remove("restaurant-appear");
             irvineYelp.classList.remove("yelp-appear");
+            irvineIcon.classList.remove("highlight");
 
             sandiego.classList.add("appear");
+            sdIcon.classList.add("highlight");
         }
     }
 })
